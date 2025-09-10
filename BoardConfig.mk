c4ebbb2e9b750fc4b7229671fd9c919aa8813661
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-09-10 15:15:04 +0200

    nuwa: Add RisingOS Flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,25 @@ include device/xiaomi/sm8550-common/BoardConfigCommon.mk
 # Inherit from the proprietary version
 include vendor/xiaomi/nuwa/BoardConfigVendor.mk
 
+# Lunch banner maintainer variable
+RISING_MAINTAINER="TheMysticle"
+
+# Chipset/Maintainer properties (ro.rising.chipset/ro.rising.maintainer) 
+# Set RISING_MAINTAINER for version control 
+# (Optional if builder is setting properties via init_<device>.cpp)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Qualcomm Snapdragon 8 Gen 2" \
+    RisingMaintainer="TheMysticle"
+
+RISING_MAINTAINER := TheMysticle
+
+# Disable/enable blur support, false by default
+TARGET_ENABLE_BLUR := true
+
+# CORE build flags
+WITH_GMS := true
+TARGET_USES_PICO_GAPPS := true
+
 # Touchscreen
 $(call soong_config_set, XIAOMI_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/class/touch/touch_dev/high_touch_rate)
 
